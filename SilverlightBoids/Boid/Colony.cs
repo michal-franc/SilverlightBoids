2dbcb7b845a814d16a9553a6769f81f898ac2cf7
LaM <lam.michal.franc@gmail.com>
2010-12-25 11:32:13 +0100

    Moved Boid Control to another Folder Created new Class representing Boid Colony.

---
+++ SilverlightBoids/Boid/Colony.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Collections.Generic;
+
+namespace SilverlightBoids.Boid
+{
+
+
+    public class Colony
+    {
+        public IList<BoidControl> Boids { get; set; }
+        public Color Color { get; set; }
+        public int BoidCounter 
+        {
+            get
+            {
+                if (Boids != null)
+                    return this.Boids.Count;
+                else
+                    return 0;
+            }
+        }
+    }
+}
